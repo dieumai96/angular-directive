8d7616e3791415e8db61c0f73cb93c9df10296cc
Vu Thuy <vuthuycntt1996@gmail.com>
2019-06-04 11:29:22 +0700

    first commit

---
+++ src/app/highlight.directive.ts
@@ -0,0 +1,30 @@
+import { Directive, ElementRef, HostListener, Input, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]'
+})
+export class HighlightDirective implements OnInit {
+  constructor(private el: ElementRef) {
+  }
+  @Input() highlightColor: string;
+  @HostListener('mouseenter') onMouseEnter() {
+
+    this.highlight('yellow');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight(null);
+  }
+
+  ngOnInit() {
+    console.log(this.highlightColor);
+    //  this.el.nativeElement.style.backgroundColor = this.highlightColor;
+
+  }
+
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.transition = '0.5s';
+    this.el.nativeElement.style.backgroundColor = color;
+  }
+}
